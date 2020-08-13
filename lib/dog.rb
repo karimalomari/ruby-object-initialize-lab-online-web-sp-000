e5dcc0597068cee598cad775cbd4c4e9ec2c6eee
Karim Alomari <karim.alomari@gmail.com>
2020-08-13 02:05:22 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,13 +1,6 @@
 class Dog
-  def initialize(name)
-    @name = name
-  end
- 
-  def name=(name)
-    @name = name
-  end
- 
-  def name
-    @name
-  end
 end
+
+fido = Dog.new
+snoopy = Dog.new
+lassie = Dog.new
