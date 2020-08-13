39b148e3486db8b05d41597a773031a1bf5206d5
Karim Alomari <karim.alomari@gmail.com>
2020-08-13 02:11:10 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,18 +1,7 @@
 class Dog
-
-  def name=(name)
+  attr_accessor :name, :breed
+  def initialize(name, breed = "Mutt")
     @name = name
-  end
-
-  def name
-    @name
-  end
-
-  def breed=(breed)
     @breed = breed
-  end
-
-  def breed
-    @breed
   end
 end
